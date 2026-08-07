60f38227ec9ea3e63133d2ac359583e56141dd44
unknown <mrrsaki0403@gmail.com>
2026-08-07 14:14:12 +0700

    Add : Category Icon Upload , Fix : Switch from flyway to JPA only

--- src/main/resources/db/migration/V20260731_03__allow_multiple_accepted_solutions.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX IF EXISTS public.uq_solutions_one_accepted_per_problem;
--- src/main/resources/db/migration/V20260802_02__program_public_statistics_index.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_reports_program_reporter
-    ON public.reports (program_id, reporter_id);
